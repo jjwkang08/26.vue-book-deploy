e8a2771c06020bfeeebf201af193f991234b16e7
jjwkang08 <jjjw8989@naver.com>
2021-10-21 20:16:55 +0900

    app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use('/', express.static(path.join('./dist')));
+app.use('/', express.static(path.join(__dirname, './dist')));
 
 app.listen(3200, () => { console.log('http://127.0.0.1:3200') });
